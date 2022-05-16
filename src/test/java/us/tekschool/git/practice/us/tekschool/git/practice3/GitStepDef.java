4df935778fa46c47c9f9534c8aa9a78bca648a70
Nasratulhaq <nasrtulhaq.fazli@tekschool.us>
2022-05-16 11:44:10 -0600

    this is nasrat

---
+++ src/test/java/us/tekschool/git/practice/us/tekschool/git/practice3/GitStepDef.java
@@ -0,0 +1,10 @@
+package us.tekschool.git.practice.us.tekschool.git.practice3;
+
+public class GitStepDef {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HNidlidf");
+	}
+
+}
